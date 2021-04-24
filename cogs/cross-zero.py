f0dfe108f9ed618d62bd930509edf41386351f9b
Shkiper228 <57232917+Shkiper228@users.noreply.github.com>
2021-04-24 23:54:34 +0300

    make it better

--- cogs/cross-zero.py
+++ cogs/cross-zero.py
@@ -12,7 +12,7 @@ from discord.utils import get
 import asyncio
 
 
-cross_zero_numbers = [[None, None, None], [None, None, None], [None, None, None]]
+cross_zero_numbers = [['#', '#', '#'], ['#', '#', '#'], ['#', '#', '#']]
 duelists = [None, None]
 
 class User(commands.Cog):
@@ -21,11 +21,12 @@ class User(commands.Cog):
 		self.client = client
 
 
-	@commands.command(aliases = ['tttcreate'])
+	@commands.command(aliases = ['tttcreate', 'tttadd'])
 	
-	async def __tic_tac_toe_create(self, ctx, duelist1, duelist2):
+	async def __tic_tac_toe_create(self, ctx, duelist1=None, duelist2=None):
 		guild = ctx.message.guild
 		channel = ctx.message.channel
+
 
 		duelists[0] = guild.get_member_named(duelist1)
 		duelists[1] = guild.get_member_named(duelist2)	 
@@ -36,24 +37,50 @@ class User(commands.Cog):
 			await channel.send(embed = discord.Embed(description = f'Гру створено\n{duelists[0]} - X\n{duelists[1]} - O'))
 
 
+	@commands.command(aliases = ['tttremove', 'tttdelete', 'tttdestroy'])
+
+	async def __tic_tac_toe_delete(self, ctx, duelist1=None, duelist2=None):
+		guild = ctx.message.guild
+		channel = ctx.message.channel
+
+
+		duelists[0] = None
+		duelists[1] = None
+
+		cross_zero_numbers = [['#', '#', '#'], ['#', '#', '#'], ['#', '#', '#']]
+		await channel.send(embed = discord.Embed(description = 'Гру видалено'))
+
+
 
 
 	@commands.command(aliases = ['cn', 'tictactoe', 'ttt', 'tic-tac-toe', 'cz', 'cross-zero', 'crosszero'])
 	
-	async def __tic_tac_toe(self, ctx, x = 1, y = 1):
+	async def __tic_tac_toe(self, ctx, x=1, y=1):
 		channel = ctx.message.channel
+		author = ctx.message.author
+		char = None
 
 		if duelists[0] == None:
 			await channel.send(embed = discord.Embed(description = f'Гра не розпочата. Щоб розпочати гру введіть команду $tttcreate [гравець1] [гравець2]'))
 			return
-			
+
+		if author == duelists[0]:
+			char = 'X'
+		elif author == duelists[1]:
+			char = 'O'
+		else:
+			await channel.send(embed = discord.Embed(description = f'{author.mention} ти не учасник гри'))
+			return
+
+		
+
 		if ctx.message.author not in duelists:
 			return
 
 		string = ''
 
-		if cross_zero_numbers[x-1][y-1] == None:
-			cross_zero_numbers[x-1][y-1] = 'X' 
+		if cross_zero_numbers[y-1][x-1] == '#':
+			cross_zero_numbers[y-1][x-1] = char 
 
 
 		#matrix
