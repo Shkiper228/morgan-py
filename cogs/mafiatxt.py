96bacb8326307c0d71d655bc9cee698b06f3bd15
Shkiper228 <57232917+Shkiper228@users.noreply.github.com>
2021-06-21 23:41:25 +0300

    make it better

--- cogs/mafiatxt.py
+++ cogs/mafiatxt.py
@@ -544,7 +544,7 @@ class User(commands.Cog):
 			perm = False
 			i = 0
 			while i < mafia_game.count:
-				if mafia_game.players[i].member.discriminator == ctx.message.author.discriminator and mafia_game.players[i].member.name == ctx.message.author.name:
+				if mafia_game.players[i].member.discriminator == ctx.message.author.discriminator:
 					perm = True
 
 				i += 1
